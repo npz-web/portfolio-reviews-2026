38a4dae3d59410ceb9a5dbf10b997d9853981169
Shauna Keating <shauna@truss.works>
2026-02-22 19:47:41 -0500

    fixed linking issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://npz-web.github.io",
-  base: "/portfolio-reviews-2026",
+  base: "/portfolio-reviews-2026", // <-- Make sure the "s" is here!
 });
